12b9e0e4d3274f3d15d9db20671328cebef8c1b1
Mouracfs <danielmourajuve10@gmail.com>
2023-09-07 18:50:41 -0300

    Criando interfaces do projeto e configurando módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,10 +24,7 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     NewMomentComponent,
     MomentFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
