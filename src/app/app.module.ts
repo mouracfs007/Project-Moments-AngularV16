fe307fdf4b205cc70a160250b9f680ea2ce04c48
Mouracfs <danielmourajuve10@gmail.com>
2023-09-07 21:45:12 -0300

    Validação de formulário com Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
